c0be330950b48dea5fb4038eba50a009761ab735
syaukhul <syaukhul@gmail.com>
2022-03-13 22:28:52 +0700

    project panggil module go_say_hello v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/syaukhul08/app-say-hello.git
 
 go 1.17
 
-require github.com/syaukhul08/go-say-hello v1.0.0 // indirect
+require github.com/syaukhul08/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/syaukhul08/go-say-hello"
+	go_say_hello "github.com/syaukhul08/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Print(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Syaukhul"))
 
 }
